856b7ecd49478f34005f73a41bf8087e82cb9efd
Shahmeer09 <shahmeerrizwan921@gmail.com>
2023-11-06 15:59:29 +0500

    LOOPS - 1 JS

---
+++ app.js
@@ -0,0 +1,43 @@
+// question 1
+
+//  for (let i = 0; i < 5; i++){
+//      document.write('Hello to the World of Loops <br>')
+//  }
+
+//question 2
+
+// for (let i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+//question 3
+
+// const number = +prompt('Enter any number to write its multiplication table:')
+// const length = +prompt('Enter the lenght of multiplication table:')
+
+// if (!isNaN(number) && !isNaN(length)){
+//     document.write(`<h2>Multiplication Table of ${[number]} </h2>`)
+//     document.write('<ul>')
+//     for (let i = 1; i <= length; i++){
+//         const result = number * i;
+//         document.write(`<li>${[number]} x ${[i]} = ${[result]} </li>`)
+//     }
+//     document.write('</ul>')
+// } else {
+//     document.write(`<h2>Please enter a valid input number</h2>`)
+// }
+
+//question 4
+
+// let A = ["Nokia", "Samsung", "Apple", "Sony", "Huawei"];
+// for (let i = 0; i < A.length; i++){
+//     console.log(A[i])
+// }
+
+
+//question 5
+
+// let fruits = ['apple', 'banana', 'mango', 'orange', 'strawberry']
+// for (let i = 0; i < fruits.length; i++){
+//     console.log(fruits[i]);
+// }

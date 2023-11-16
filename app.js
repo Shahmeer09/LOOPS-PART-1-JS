18a5afee7328a2f7003dfc9f14e1fc3ca12ed528
shahmeer09 <shahmeerrizwan921@gmail.com>
2023-11-16 19:56:26 +0500

    practice

--- app.js
+++ app.js
@@ -419,3 +419,18 @@
 //      // Display an error message if the input is not a valid number
 //      console.log("Invalid input. Please enter a valid number.");
 //  }
+
+
+
+// ANOTHER QUES ;
+
+
+// let A = [24, 53, 78, 91, 12]
+// let smallestNum = A[0]
+
+// for (let i = 1; i < A.length; i++){
+//     if (A[i] < smallestNum){
+//         smallestNum = A[i]
+//     }
+// }
+// console.log(`The smallest number in the Array is ${[smallestNum]}`);

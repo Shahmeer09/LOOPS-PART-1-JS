124dd9ce68fa79a98a1d0c75bf43faa14a9451ab
shahmeer09 <shahmeerrizwan921@gmail.com>
2023-11-18 19:38:43 +0500

    practice

--- app.js
+++ app.js
@@ -461,48 +461,51 @@
 
     // console.log(user.girlfriend);
 
-    let complexObject = {
-        person: {
-            name: {
-                first: "John",
-                last: "Doe",
-            },
-            age: 30,
-            address: {
-                street: {
-                    number: 123,
-                    name: "Nested Street",
-                },
-                city: "Nested City",
-                country: "Nested Country",
-            },
-        },
-        work: {
-            company: {
-                name: "TechCorp",
-                location: {
-                    city: "TechCity",
-                    country: "TechCountry",
-                },
-            },
-            position: "Senior Developer",
-            projects: [
-                {
-                    name: "Project A",
-                    technologies: ["JavaScript", "React", "Node.js"],
-                },
-                {
-                    name: "Project B",
-                    technologies: ["Python", "Django", ["new" , ["PostgreSQL"]]],
-                },
-            ],
-        },
-        hobbies: {
-            indoor: ["Reading", "Chess"],
-            outdoor: ["Hiking", "Cycling"],
-        },
-    };
+    // let complexObject = {
+    //     person: {
+    //         name: {
+    //             first: "John",
+    //             last: "Doe",
+    //         },
+    //         age: 30,
+    //         address: {
+    //             street: {
+    //                 number: 123,
+    //                 name: "Nested Street",
+    //             },
+    //             city: "Nested City",
+    //             country: "Nested Country",
+    //         },
+    //     },
+    //     work: {
+    //         company: {
+    //             name: "TechCorp",
+    //             location: {
+    //                 city: "TechCity",
+    //                 country: "TechCountry",
+    //             },
+    //         },
+    //         position: "Senior Developer",
+    //         projects: [
+    //             {
+    //                 name: "Project A",
+    //                 technologies: ["JavaScript", "React", "Node.js"],
+    //             },
+    //             {
+    //                 name: "Project B",
+    //                 technologies: ["Python", "Django", ["new" , ["PostgreSQL"]]],
+    //             },
+    //         ],
+    //     },
+    //     hobbies: {
+    //         indoor: ["Reading", "Chess"],
+    //         outdoor: ["Hiking", "Cycling"],
+    //     },
+    // };
 
 
-    console.log(complexObject.work.company.location.country);
-    console.log(complexObject.work.projects[1].technologies[2][1][0]);
+    // console.log(complexObject.work.company.location.country);
+    // console.log(complexObject.work.projects[1].technologies[2][1][0]);
+
+
+    

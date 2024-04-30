da766337c42297b180db40cad91b4aeb2aa0d418
Muhammad Shahmeer Rizwan <148860820+shahmeerrizwan@users.noreply.github.com>
2024-04-30 21:53:37 +0500

    practice

--- javascript practice/app.js
+++ javascript practice/app.js
@@ -2788,3 +2788,65 @@ alert(x + y)
 // console.log(`The Array is ${[A]}`)
 // console.log("The largest number in the array is: " + largeNum)
 // console.log("The smallest number in the array is: " + SmallNum)
+
+
+
+
+
+
+
+
+// IF ELSE QUESTIONS 
+
+
+//question 1
+
+// let number = prompt("Enter a number:");
+
+// if (!isNaN(number)) {
+//     if (number % 3 === 0) {
+//         console.log(number + " is divisible by 3.");
+//     } else {
+//         console.log(number + " is not divisible by 3.");
+//     }
+// } else {
+//     console.log("Invalid input. Please enter a valid number.");
+// }
+
+
+//question 2
+
+// let number = prompt("Enter a number:");
+
+// if (!isNaN(number)) {
+//     if (number % 2 === 0) {
+//         console.log(number + " is an even number.");
+//     } else {
+//         console.log(number + " is an odd number.");
+//     }
+// } else {
+//     console.log("Invalid input. Please enter a valid number.");
+// }
+
+
+//question 3
+
+//  let age = 16; 
+
+//  if (age > 18) {
+//      console.log("Old enough");
+//  } else {
+//      console.log("Too young");
+//  }
+
+
+//question 4
+
+//  let userName = prompt("Enter your name:");
+
+//  if (userName = "Shahmeer") {
+//      console.log("Hello, Shahmeer! It's great to see you!");
+//  } else {
+
+//  }
+

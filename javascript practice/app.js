d6109b025a66a60381003ef507c82109ea52f8f1
Muhammad Shahmeer Rizwan <148860820+shahmeerrizwan@users.noreply.github.com>
2024-05-05 23:21:16 +0500

    practice

--- javascript practice/app.js
+++ javascript practice/app.js
@@ -3569,3 +3569,39 @@ alert(x + y)
 //     document.write(line + '<br>');
 // }
 
+
+
+// Array Assignments  
+
+
+//question 1
+
+//   let studentNames = [];
+
+//  studentNames.push("Abdullah");
+//  studentNames.push("Adnan");
+//  studentNames.push("Talha");
+
+//  console.log(studentNames);
+
+//question 2
+
+//  let studentNames = new Array();
+//  let studentNames = [];
+
+
+//question 3
+
+
+// let colors = ["red", "green", "blue", "yellow"];
+
+
+//question 4
+
+// let numbers = [1, 2, 3, 4, 5];
+
+
+//question 5
+
+//  let flags = [true, false, true, false, true];
+

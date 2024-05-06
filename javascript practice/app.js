35c906a8913801b1676a5dd1963599dd31892d7e
Muhammad Shahmeer Rizwan <148860820+shahmeerrizwan@users.noreply.github.com>
2024-05-06 23:31:15 +0500

    practice

--- javascript practice/app.js
+++ javascript practice/app.js
@@ -3698,3 +3698,146 @@ console.log("Car at last index of the array: " + favoriteCars[favoriteCars.lengt
 //      console.log("Invalid input. Please enter a valid number.");
 //  }
 
+
+
+// Object Rendering In browser :
+
+
+
+
+
+
+
+let mobiles = obj = {
+    iphone: {
+        iphone7: {
+            Screen: "4.2 inches",
+            Ram: "4gb",
+            Rom: "64gb"
+        },
+        iphone8: {
+            Screen: "4.3 inches",
+            Ram: "4gb",
+            Rom: "64gb"
+        },
+        iphone9: {
+            Screen: "4.4 inches",
+            Ram: "4gb",
+            Rom: "64gb"
+        },
+        iphone10: {
+            Screen: "4.5 inches",
+            Ram: "4gb",
+            Rom: "64gb"
+        },
+        iphone11: {
+            Screen: "4.8 inches",
+            Ram: "6gb",
+            Rom: "128gb"
+        },
+        iphone12: {
+            ram: "8gb",
+            rom: "256gb"
+        },
+        iphone13: {
+            ram: "8gb",
+            rom: "256gb"
+        }
+    },
+    Redmi: {
+        Redmi13: {
+            Screen: "4.5 inches",
+            Ram: "4gb",
+            Rom: "64gb"
+        },
+        RedmiA: {
+            Screen: "4.9 inches",
+            Ram: "6gb",
+            Rom: "128gb"
+        },
+        RedmiNote13: {
+            Screen: "5.1 inches",
+            Ram: "8gb",
+            Rom: "256gb"
+        },
+        RedmiNote13Pro: {
+            Screen: "5.3 inches",
+            Ram: "8+8gb",
+            Rom: "512gb"
+        },
+    },
+    Xiomi: {
+        Xiomi11T: {
+            Screen: "4.8 inches",
+            Ram: "4gb",
+            Rom: "64gb"
+        },
+        Xiomim1: {
+            Screen: "4.9 inches",
+            Ram: "6gb",
+            Rom: "128gb"
+        },
+        Xiomim2: {
+            Screen: "5.1 inches",
+            Ram: "8gb",
+            Rom: "256gb"
+        },
+        Xiomi13: {
+            Screen: "5.4 inches",
+            Ram: "8+8gb",
+            Rom: "512gb"
+        },
+    },
+    Realmi: {
+        Realmec51: {
+            Screen: "4.8 inches",
+            Ram: "4+4gb",
+            Rom: "128gb"
+        },
+        Realmec52: {
+            Screen: "5.0 inches",
+            Ram: "6gb",
+            Rom: "128gb"
+        },
+        Realmec53: {
+            Screen: "5.1 inches",
+            Ram: "6gb",
+            Rom: "128gb"
+        },
+        Realme13C: {
+            Screen: "5.8 inches",
+            Ram: "8+8+5gb",
+            Rom: "512gb"
+        },
+    }
+}
+
+let brand = document.querySelector("#brands")
+let model = document.querySelector("#models")
+
+let allBrand = Object.keys(mobiles)
+
+function init() {
+    for (let i = 0; i < allBrand.length; i++) {
+        brand.innerHTML += `<option>${allBrand[i]}</option>`
+    }
+}
+init()
+
+function selectBrand() {
+    let selectedBrand = brand.value
+    let models = Object.keys(mobiles[selectedBrand])
+
+    model.innerHTML = ""
+    for (let i = 0; i < models.length; i++) {
+        model.innerHTML += `<option>${models[i]}</option>`
+    }
+}
+
+function modelSelect() {
+    let selectedBrand = brand.value
+    let selectedModel = model.value
+    let val = mobiles[selectedBrand][selectedModel]
+    console.log(val)
+}
+init()

101179dd1abc733eee6dd74aec6ba0a2821ae32b
Muhammad Shahmeer Rizwan <148860820+shahmeerrizwan@users.noreply.github.com>
2024-05-07 22:37:10 +0500

    practice

--- javascript practice/app.js
+++ javascript practice/app.js
@@ -3949,3 +3949,61 @@ init()
 // }
 
 //                    Correct
+
+
+
+
+// B)
+// var b = 82;
+// if (b++ === 83) {
+//     alert("given condition for variable b is true");
+// }
+
+//                    Incorrect // False
+
+
+// C)
+
+// var c = 12;
+// if (c++ === 13) {
+//     alert("condition 1 is true"); // FALSE
+// }
+// if (c === 13) {
+//     alert("condition 2 is true"); // TRUE
+// }
+// if (++c < 14) {
+//     alert("condition 3 is true"); // FALSE
+// }
+// if (c === 14) {
+//     alert("condition 4 is true");  // TRUE
+// }
+
+
+// D)
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost) {
+//     alert("The cost equals");
+// }
+
+//               Correct 
+
+
+// E)
+
+// if (true) {
+//     alert("True");  // TRUE
+// }
+// if (false) {
+//     alert("False"); //FALSE 
+// }
+
+
+// F)
+
+// if ("car" < "cat") {
+//     alert("car is smaller than cat");  // TRUE
+// }
+

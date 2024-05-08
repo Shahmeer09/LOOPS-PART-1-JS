fb6d1ff295a4f6fdd33ec611575ec47305f981db
Muhammad Shahmeer Rizwan <148860820+shahmeerrizwan@users.noreply.github.com>
2024-05-08 22:50:20 +0500

    practice

--- javascript practice/app.js
+++ javascript practice/app.js
@@ -4100,3 +4100,28 @@ Shipping Charges ${shipping} .
 Total Cost Of Your Order Is ${finalPrice} PKR .<br>
 Discount Price After 10% Discount is ${price} PKR Save Rupess is ${saveRupee} .
 `)
+
+
+// CONDITIONAL RENDERING 
+
+// QUESTION NO 1 :
+
+
+// let name = prompt("Enter Your Name ");
+// alert(`Hi, ${name}`)
+
+
+// QUESTION NO 2 :
+
+
+// let number = prompt("Enter a number you want to generate table");
+// let table = "<h3>Multiplication Table of " + number + "</h3>";
+
+// if (number == "") {
+//     number = 5;
+// }
+// for (let i = 1; i <= 10; i++) {
+//     table += number + " × " + i + " = " + (number * i) + "<br>";
+// }
+
+// document.write(table);

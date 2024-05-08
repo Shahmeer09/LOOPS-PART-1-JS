87817086064a1e715581b8c6f8715f552c6c8a96
Muhammad Shahmeer Rizwan <148860820+shahmeerrizwan@users.noreply.github.com>
2024-05-08 22:50:44 +0500

    practice

--- javascript practice/app.js
+++ javascript practice/app.js
@@ -4125,3 +4125,49 @@ Discount Price After 10% Discount is ${price} PKR Save Rupess is ${saveRupee} .
 // }
 
 // document.write(table);
+
+
+
+// QUESTION NO 3 :
+
+
+// let cityName = prompt("Enter Your City Name ").toLowerCase();
+
+// if (cityName == "karachi") {
+//     alert("Welcome to City of Lights")
+// }
+
+
+
+// QUESTION NO 4 :
+
+
+// let gender = prompt("Enter Your Gender ").toLowerCase();
+
+// if (gender == "male") {
+//     alert("Good Morning Sir");
+// } else if (gender == "female") {
+//     alert("Good Morning Ma'am")
+// } else {
+//     alert("Please Enter the Correct Gender");
+// }
+
+
+
+// QUESTION NO 5 :
+
+
+// let color = prompt("Enter a Traffic Signal Color").toLowerCase();
+
+// if (color == "red") {
+//     alert("Vehicles Must Stop")
+// } else if (color == "yellow") {
+//     alert("Vehicles Should Get Ready To Move")
+// } else if (color == "green") {
+//     alert("Vehicles can move now")
+// } else {
+//     alert("Enter Only Traffic Signal Color")
+// }
+
+
+

50f2fa24590f79101e30d05ce13a0dad4e652a37
Muhammad Shahmeer Rizwan <148860820+shahmeerrizwan@users.noreply.github.com>
2024-05-09 23:18:16 +0500

    practice

--- javascript practice/app.js
+++ javascript practice/app.js
@@ -4354,3 +4354,50 @@ Shipping Charges ${shipping} .
 Total Cost Of Your Order Is ${finalPrice} PKR .<br>
 Discount Price After 10% Discount is ${price} PKR Save Rupess is ${saveRupee} .
 `)
+
+
+
+
+// STOP WATCH 
+
+
+let milisec = 0;
+let seconds = 0;
+let minutes = 0;
+let myTimeInterval = null
+let getHtmlmsec = document.getElementById('msec');
+let getHtmlsec = document.getElementById('sec');
+let getHtmlmin = document.getElementById('min');
+
+
+function start() {
+    myTimeInterval = setInterval(function () {
+        milisec++;
+        getHtmlmsec.innerHTML = " : " + milisec + " msec ";
+        if (milisec >= 100) {
+            seconds++;
+            getHtmlsec.innerHTML = " : " + seconds + " sec ";
+            milisec = 0;
+        }
+        if (seconds >= 60) {
+            minutes++;
+            getHtmlmin.innerHTML = minutes + " min ";
+            seconds = 0;
+        }
+    }, 10);
+
+}
+
+
+function stop() {
+    clearInterval(myTimeInterval);
+}
+
+function clearTimer() {
+    milisec = 0;
+    seconds = 0;
+    minutes = 0;
+    getHtmlmsec.innerHTML = " : 00 ";
+    getHtmlsec.innerHTML = " : 00 ";
+    getHtmlmin.innerHTML = "00 ";
+}
